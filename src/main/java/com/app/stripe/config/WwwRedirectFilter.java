0da5169fcf5d15d1c7e96d1a2326160b1357af18
Diego Alejandro Crognale <diego.crognale@tenpo.cl>
2026-04-28 22:41:14 -0300

    77

--- src/main/java/com/app/stripe/config/WwwRedirectFilter.java
+++ src/main/java/com/app/stripe/config/WwwRedirectFilter.java
@@ -12,7 +12,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Slf4j
-@Component
 public class WwwRedirectFilter extends OncePerRequestFilter {
 
   @Override
